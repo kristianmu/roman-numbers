828ac7b2ec4d812820d4bc9a5229ac6e3ab0a21a
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-03 11:15:57 +0100

    convert umber 3

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -1,5 +1,9 @@
 public class RomanNumeralsConverter {
     public String toRoman(int decimal) {
+        if(decimal == 3){
+            return "III";
+        }
+
         if (decimal == 2) {
             return "II";
         }
