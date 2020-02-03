f088b5cfacb2eb3ed07f7a1110923adc5726d025
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-03 11:18:20 +0100

    refactor

--- src/test/java/RomanNumbersShould.java
+++ src/test/java/RomanNumbersShould.java
@@ -1,32 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RomanNumbersShould {
 
+    private RomanNumeralsConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new RomanNumeralsConverter();
+    }
+
     @Test
     public void convert_number_one_to_I(){
-        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
-
-        String result = romanNumeralsConverter.toRoman(1);
+        String result = converter.toRoman(1);
 
         assertEquals( "I", result);
     }
 
     @Test
     public void convert_number_two_to_II() {
-        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
-
-        String result = romanNumeralsConverter.toRoman(2);
+        String result = converter.toRoman(2);
 
         assertEquals( "II", result);
     }
 
     @Test
     public void convert_number_three_to_III() {
-        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
-
-        String result = romanNumeralsConverter.toRoman(3);
+        String result = converter.toRoman(3);
 
         assertEquals( "III", result);
     }
